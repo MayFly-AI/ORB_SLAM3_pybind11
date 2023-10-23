b32cd1cca1a8fb3c60383219585ea24d192b8b6f
maxlacourchristensen <max@mayfly.dk>
2023-10-23 14:06:37 +0200

    changed videocapture to sensorcapture

--- Examples_python/sensorleap.py
+++ Examples_python/sensorleap.py
@@ -7,7 +7,7 @@ import cv2
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../lib') # .so file needs to be in sys.path
 import pyorbslam3
 
-from mayfly.videocapture import VideoCapture
+from mayfly.sensorcapture import SensorCapture
 
 def main():
     if len(sys.argv) < 3:
@@ -17,13 +17,15 @@ def main():
     SLAM = pyorbslam3.System(sys.argv[1],sys.argv[2],pyorbslam3.System.eSensor.MONOCULAR,True,0,'')
     imageScale = SLAM.GetImageScale()
 
-    cap = VideoCapture(list(range(64)), '')
+    cap = SensorCapture(list(range(64)), '')
     tframe = 1694088977000*1e6
     frame_time_ns = int((1000./30.)*1.e6)
     frame_idx = 0
     while True:
-        frames = cap.read()
-        frm = frames[0] # It may have more than 1 frame if sync cameras or ToF. We assume 1 frame
+        capture = cap.read()
+        if capture['type'] != 'camera':
+            continue
+        frm = capture['frames'][0] # It may have more than 1 frame if sync cameras or ToF. We assume 1 frame
         arr = np.from_dlpack(frm['image']).copy()
         bgr = cv2.cvtColor(arr[:,:,:3], cv2.COLOR_RGB2BGR)
         im = cv2.cvtColor(arr[:,:,:3], cv2.COLOR_RGB2GRAY)
